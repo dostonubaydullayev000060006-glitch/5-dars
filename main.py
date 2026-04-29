fa0e97fe548ba9c7b3a37e867d2abd1a22477054
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-04-29 17:44:40 +0500

    darsdagi kod

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+os.system("cls")
+
+car = {
+    "brend":" Chevrolet",
+    "model": "Coblt",
+    "rang": 'oq',
+    'narx': 12000,
+    'probeg': 80_000,
+    'rang': "qora"
+}
+
+# car["brend"] = "Gm"
+# car['yil'] = 2023
+# print(car['model'],car['brend'],sep=", ")
+# print(car)
+
+# if "rang" in car:
+#     print("Yes ")
+# else: 
+#     print("NO ")
+
+# for n in car:
+#     print(n ,car[n])
+
+
+                                        # METODLARI
+
+                                        
+# print(car["yil"])
+# print(car.get('yil', "topilmadi"))
+
+# print(list(car.keys()))
+
+# print(list(car.values()))
+
+# k = car.pop("probeg")
+# print(car)
+# print(k)
+
+# car2 = car.copy()
+# print(car2)
+
+# print(car.items())
+# for i,j in car.items():
+#     print(i, j)
+
+# car.clear()
+# print(car)
+
+# k = car.popitem()
+# print(k)
+# print(car)
+
+# car['narx'] = 9000
+# car['ish-turi'] = 'dastavka'
+# car['probeg'] = 100000
+
+# car.update({"narx": 9000, 'ish-turi':'dastavka', 'probeg':100000})
+# print(car)
