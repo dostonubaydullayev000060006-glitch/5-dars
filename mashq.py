bdc797a9580bba6d1ffb3cc8645bc872585f637b
Doston <dostonubaydullayev00006.0006@gmail.com>
2026-04-29 17:45:03 +0500

    Uy ishlari

---
+++ mashq.py
@@ -0,0 +1,73 @@
+import os
+os.system("cls")
+
+# 1.m
+# students = {
+# 	"Samandar": 18,
+# 	"Muzaffar": 19,
+# 	"Xojiakbar": 16,
+# 	"Islom": 20,
+# 	"Asomiddin": 14,
+# 	"Sobitjon": 17,
+# 	"Shoxruh": 20
+# }
+# for i,j in students.items():
+#     if j>=18:
+#         print(i)
+
+
+# 2.m
+# users = {
+# 	"jeymsBond" : "agent007",
+# 	"tony_stark": "ironman101",
+#  	"piterParker": "spider.12.12",
+# 	"sher": "1"
+# 	}
+
+# a = input("User name kriting: ")
+# b = input("Parol kriting: ")
+
+# if a not in users:
+#     print("Bunday foydalanuvchi yoq: ")
+# elif users[a]==b:
+#     print("Hisobga krdingiz.")
+# else:
+#     print("parol notug'ri!")
+
+
+# 3.m
+# import os
+# os.system("cls")
+
+# list1=['S001', 'S002', 'S003', 'S004']
+# list2=['Adina Park', 'Leyton Marsh', 'Duncan Boyle', 'Saim Richards']
+# list3=[85, 98, 89, 92]
+# list4=[]
+
+
+# for i in range(4):
+#     list4.append({list1[i]: {list2[i]: list3[i]}})
+    
+# print(list4)
+
+
+# 4.m
+# l = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# dic = {}
+
+# for key, value in l:
+#     dic.setdefault(key, []).append(value)
+
+# print(dic)
+
+# 5.m
+# import os
+# data= {
+#     'Math': [88, 89, 90], 
+#      'Physics': [92, 94, 89], 
+#      'Chemistry': [90, 87, 93]
+#      }
+
+
+# data.update({'Math': [91, 92, 93], 'Physics': [95, 97, 92], 'Chemistry': [93, 90, 96]})
+# print(data)
